76c9df5217d1354578596a511c49e0d4bbfce655
Eddington Rowellis <eddingtonibi@gmail.com>
2023-01-19 15:22:29 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+    return fetch('https://anapioficeandfire.com/api/books')
+        .then(response => response.json())
+        .then(json => renderBooks(json))
 }
 
 function renderBooks(books) {
-  const main = document.querySelector('main');
-  books.forEach(book => {
-    const h2 = document.createElement('h2');
-    h2.innerHTML = book.name;
-    main.appendChild(h2);
-  });
+    let main = document.querySelector('main')
+    books.forEach(book => {
+        let title = document.createElement('h2')
+        title.innerText = book.name
+        main.appendChild(title)
+    })
 }
-
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
-});
